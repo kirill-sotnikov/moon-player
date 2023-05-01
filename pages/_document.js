411563bc59bb1a93eb541ca4ed0ed2d73cc2de40
Kirill Sotnikov <sot-ks@ya.ru>
2023-05-01 14:59:01 +0300

    PWA app

---
+++ pages/_document.js
@@ -0,0 +1,69 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+          <meta name="application-name" content="PWA App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
+          <meta name="apple-mobile-web-app-title" content="PWA App" />
+          <meta name="description" content="Best PWA App in the world" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#1E2439" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#1E2439" />
+
+          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+          <link rel="manifest" href="/manifest.json" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://yourdomain.com" />
+          <meta name="twitter:title" content="PWA App" />
+          <meta
+            name="twitter:description"
+            content="Best PWA App in the world"
+          />
+          <meta
+            name="twitter:image"
+            content="https://yourdomain.com/icons/android-chrome-192x192.png"
+          />
+          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="PWA App" />
+          <meta property="og:description" content="Best PWA App in the world" />
+          <meta property="og:site_name" content="PWA App" />
+          <meta property="og:url" content="https://yourdomain.com" />
+          <meta
+            property="og:image"
+            content="https://yourdomain.com/icons/apple-touch-icon.png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
